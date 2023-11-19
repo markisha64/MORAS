f33aaa36d3c422448a7312bc4c455252e880c553
Marko Grizelj <marko.grizelj@gauss.dev>
2023-11-19 15:53:29 +0100

    feat: initial code needed for set x y

---
+++ zadaca_2/z3.asm
@@ -0,0 +1,161 @@
+
+// definiraj registre
+@A0
+@A1
+@A3
+@RA
+@RV
+@V0
+@V1
+@V2
+@V3
+
+// namjesti konstante
+@15
+D = A;
+@B
+M = D;
+
+@240
+D = A;
+@B
+A = A + 1;
+M = D;
+
+@3840
+D = A;
+@B
+A = A + 1;
+A = A + 1;
+M = D;
+
+@61440
+D = A;
+@B
+A = A + 1;
+A = A + 1;
+A = A + 1;
+M = D;
+
+// odi u main
+@MAIN
+0; JMP
+
+(MUL)
+        // v0 iterator
+        @V0
+        M = D;
+
+        // v1 rjesenje
+        @V1
+        M = 0;        
+
+        (MUL_LOOP)
+                @V0
+                D = M;
+                @MUL_LOOP_END
+                D; JEQ
+
+                @A0
+                D = M;
+                @V1
+                M = M + D;
+
+                @V0
+                M = M - 1; 
+
+                @MUL_LOOP
+                0; JMP
+
+        (MUL_LOOP_END)
+        @V1
+        D = M;
+        @RV
+        M = D;
+        @RA
+        D = M;
+        A = D;
+        0; JMP         
+
+
+(DIV)
+        // rjesenje i iterator
+        @V0
+        M = 0;
+      
+        // ucitaj a0 u v1
+        @A0
+        D = M;
+        @V1
+        M = D;
+         
+        (DIV_LOOP)
+                @V1 
+                D = M;
+                @DIV_LOOP_END
+                D; JLT
+
+                @A1
+                D = M;
+                @V1
+                M = M - D;
+                @V0
+                M = M + 1;
+
+                @DIV_LOOP
+                0; JMP
+
+        (DIV_LOOP_END)
+        @V1
+        D = M;
+        @DIV_ADD_1_END
+        D; JEQ
+        
+        (DIV_ADD_1)
+        @V0
+        M = M - 1;
+
+        (DIV_ADD_1_END)
+        @V0
+        D = M;
+        @RV
+        M = D;
+        @RA
+        D = M;
+        A = D;
+        0; JMP
+              
+
+(MAIN)
+        @B
+        D = M;
+
+        @44
+        D = A;
+        @A0
+        M = D;
+
+        @2
+        D = A;
+        @A1
+        M = D;
+
+        @t0
+        D = A;
+        @RA
+        M = D;
+
+        @DIV
+        0; JMP
+
+        (t0)
+        @RV
+        D = M;
+        @0
+        M = D;
+
+
+        (MAIN_END)
+                @MAIN_END
+                0; JMP
+       
